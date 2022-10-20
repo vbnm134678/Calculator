2e90a51ad75d93cc443497cdab3d080d3a9fc304
vbnm134678 <76089508+vbnm134678@users.noreply.github.com>
2022-10-20 16:47:48 +0900

    사칙연산추가

--- src/main/java/gcukakao/jenkins/calculator/Calculator.java
+++ src/main/java/gcukakao/jenkins/calculator/Calculator.java
@@ -5,5 +5,14 @@ import org.springframework.stereotype.Service;
 public class Calculator { 
      public int sum(int a, int b) { 
           return a + b; 
+     }
+     public int sub(int a, int b) { 
+          return a - b; 
+     } 
+     public int mul(int a, int b) { 
+          return a * b; 
+     } 
+     public int div(int a, int b) { 
+          return a / b; 
      } 
 } 
