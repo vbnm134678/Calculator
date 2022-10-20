f7778d4ce1b95ec4eb1e7706492f24ef112f29f7
vbnm134678 <syj1351@naver.com>
2022-10-20 10:12:08 +0900

    f

---
+++ src/main/java/gcukakao/jenkins/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package gcukakao.jenkins.calculator; 
+import org.springframework.stereotype.Service; 
+ 
+@Service 
+public class Calculator { 
+     public int sum(int a, int b) { 
+          return a + b; 
+     } 
+} 
---
+++ src/test/java/gcukakao/jenkins/calculator/CalculatorTest.java
@@ -0,0 +1,15 @@
+package gcukakao.jenkins.calculator; 
+import org.junit.Test;
+
+import gcukakao.jenkins.calculator.Calculator;
+
+import static org.junit.Assert.assertEquals; 
+ 
+public class CalculatorTest { 
+     private Calculator calculator = new Calculator(); 
+ 
+     @Test 
+     public void testSum() { 
+          assertEquals(5, calculator.sum(2, 3)); 
+     } 
+} 
